4580b2395d90b88cf3840d5683f172a5e6474882
sauravkumal <sk.sauravkumal@gmail.com>
2019-05-23 12:15:44 +0545

    basic test

--- Basic Operation/blur.py
+++ Basic Operation/blur.py
@@ -5,10 +5,13 @@ img = cv2.imread('../data/lane.jpg')
 #print(type(img)) #checking if any error occured
 
 #Gausian Blur
-processed_img = cv2.GaussianBlur(img,(5,5),0)
-
-
-
+processed_img = cv2.GaussianBlur(img,(3,3),0)
+processed_img1 = cv2.GaussianBlur(img,(5,5),0)
+processed_img2 = cv2.GaussianBlur(img,(7,7),0)
 
 cv2.imshow('GaussianBlur',processed_img)
+cv2.waitKey(1)
+cv2.imshow('GaussianBlur1',processed_img1)
+cv2.waitKey(1)
+cv2.imshow('GaussianBlur2',processed_img2)
 cv2.waitKey(0)
