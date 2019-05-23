56507584f34fed3cf82958e8023c9113258e29c6
sauravkumal <sk.sauravkumal@gmail.com>
2019-05-23 13:01:13 +0545

    Variation with canny detection with gaussian blur

--- Basic Operation/blur.py
+++ Basic Operation/blur.py
@@ -3,15 +3,10 @@ import cv2
 
 img = cv2.imread('../data/lane.jpg')
 #print(type(img)) #checking if any error occured
-
 #Gausian Blur
-processed_img = cv2.GaussianBlur(img,(3,3),0)
-processed_img1 = cv2.GaussianBlur(img,(5,5),0)
-processed_img2 = cv2.GaussianBlur(img,(7,7),0)
-
-cv2.imshow('GaussianBlur',processed_img)
-cv2.waitKey(1)
-cv2.imshow('GaussianBlur1',processed_img1)
-cv2.waitKey(1)
-cv2.imshow('GaussianBlur2',processed_img2)
-cv2.waitKey(0)
+while(True):
+	kernel = tuple(map(int,input('Enter kernel value in format x,x :').split(',')))
+	processed_img = cv2.GaussianBlur(img,kernel,0)
+	canny_image = cv2.Canny(processed_img,50,150)
+	cv2.imshow('Blur effect in canny',canny_image)
+	cv2.waitKey(1)
